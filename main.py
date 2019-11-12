4c72db10ed4ec94e8d73d7c7d1e14ee50cc9beac
Julia Cox <41338503+astrocox@users.noreply.github.com>
2019-11-12 16:30:39 -0500

    Add skeleton main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    assert len(sys.argv) > 1, "No input file path specified."
+
+    input_file_path = sys.argv[1]
+    print(f"Processing input file: {input_file_path}")
+
+    # TODO: Fill in the actual logic here!
+
+
+if __name__ == "__main__":
+    main()
