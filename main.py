5df64399e7d87aab5f93d517db105828e3e1e04b
Samantha Wu <samiekoo@gmail.com>
2019-11-27 22:18:01 -0500

    homework

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 
 def main():
@@ -7,7 +8,16 @@ def main():
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
+    from statistics import mean
     # TODO: Fill in the actual logic here!
+    with open(input_file_path, newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            l1 = []
+            for numbers in row:
+                l1.append(float(numbers))
+            average = mean(l1)
+            print(average)
 
 
 if __name__ == "__main__":
